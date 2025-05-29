b95b8b67e179787e2f09c13b9a28d22715008c7b
Andrey <shooroop87@mail.ru>
2025-05-29 22:47:51 +0300

    v2 solution
    v2 solution

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { HotelList } from '../pages/HotelList';
+import { SignIn } from '../pages/SignIn';
+
 export const App = () => {
-	return <></>
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/hotel-list" element={<HotelList />} />
+			<Route path="/sign-in" element={<SignIn />} />
+		</Routes>
+	);
 };
